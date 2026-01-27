ece8b81d4e502ed5920204228fe1a911392dccde
DirkKuijpers <Dirk.p.Kuijpers@gmail.com>
2026-01-27 12:00:02 +0100

    De pagina voor de lessen compleet

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// script.js
+function openModal(id) {
+    document.getElementById(id).style.display = 'block';
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+// Sluit modal als je buiten klikt
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+}
