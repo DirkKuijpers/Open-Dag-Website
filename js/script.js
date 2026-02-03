42ef29876e4d0e2045a6e838174288b187c8ec0b
DirkKuijpers <Dirk.p.Kuijpers@gmail.com>
2026-02-03 10:21:11 +0100

    responsive fix

--- js/script.js
+++ js/script.js
@@ -16,3 +16,11 @@ window.onclick = function(event) {
         }
     });
 }
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
